12cbd4b297beff5e92122fb79b49797e55554207
oceanvievv <oceanvievv@protonmail.com>
2022-07-05 15:35:15 +0500

    Add normalize & reset css packages, set body styles

--- src/App.vue
+++ src/App.vue
@@ -4,4 +4,33 @@
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+// CSS optimizers
+@import "~normalize.css";
+@import "~modern-css-reset";
+
+// Fonts
+@import "@/styles/fonts/montserrat";
+@import "@/styles/fonts/roboto";
+
+// Consts
+@import "@/styles/consts/consts";
+
+body {
+  font-family: "MontserratMedium", sans-serif;
+
+  color: $base-text-color;
+
+  background-color: $background-color-light-gray;
+
+  margin-top: 70px;
+
+  @media screen and (max-width: 1110px) {
+    margin-top: 40px;
+
+    @media screen and (max-width: 375px) {
+      margin-top: 20px;
+    }
+  }
+}
+</style>
