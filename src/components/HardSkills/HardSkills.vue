d957fdd4b4bec542db10e5198cb9c2f927b8145d
oceanvievv <oceanvievv@protonmail.com>
2022-07-05 18:21:50 +0500

    FIN: Hard-skills component

---
+++ src/components/HardSkills/HardSkills.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hard-skills-wrapper bottom-middle-shadow">
+    <div class="hard-skills__title">FRONT END</div>
+    <div class="skills-wrapper" v-for="(skill, idx) in skills" :key="idx">
+      <SkillItem
+        v-bind:progress-bar-title="skill.title"
+        v-bind:progress-bar-value="skill.value"
+      ></SkillItem>
+    </div>
+  </div>
+</template>
+
+<script>
+import SkillItem from "@/components/HardSkills/SkillItem";
+
+export default {
+  name: "HardSkills",
+  components: {
+    SkillItem,
+  },
+
+  data() {
+    return {
+      skills: [
+        { title: "React", value: "60%" },
+        { title: "Javascript", value: "80%" },
+        { title: "CSS", value: "90%" },
+        { title: "Vue", value: "60%" },
+        { title: "Redux", value: "80%" },
+        { title: "React Native", value: "90%" },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+// Container shadow
+@import "@/styles/base/bottom-middle-shadow.scss";
+
+@import "hard-skills";
+</style>
---
+++ src/components/HardSkills/SkillItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hard-skills__skill">
+    <div class="skill__title">{{ progressBarTitle }}</div>
+    <div class="skill__progress-bar-wrapper">
+      <div
+        class="skill__progress-bar"
+        v-bind:style="{ width: progressBarValue }"
+      ></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SkillItem",
+  props: {
+    progressBarTitle: String,
+    progressBarValue: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "skill-item";
+</style>
